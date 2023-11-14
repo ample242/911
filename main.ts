298c27a43e0793ff948f1dd984608ac1817e219c
ample242 <150863561+ample242@users.noreply.github.com>
2023-11-15 09:45:39 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.showString("9:11")
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    # . . # .
+    # # # # #
+    # . . # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . # .
+    . . . # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    # . . # .
+    # # # # #
+    # . . # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . # .
+    . # . # .
+    . # . # .
+    `)
+basic.forever(function () {
+	
+})
